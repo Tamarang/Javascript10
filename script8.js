ac3d520a4fb3c8e30c331557dac0ea110707b701
Jan Siepel <91777292+Tamarang@users.noreply.github.com>
2021-12-29 11:09:34 +0100

    Add files via upload

---
+++ script8.js
@@ -0,0 +1,46 @@
+  const AgeCheck = (Age) => {
+    if(Age >= 18)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  };
+  const Greet = () => {
+    let ageperson = AgeCheck.call(this, 18)
+    if(ageperson == true)
+    {
+      return "Hello there";
+    }
+    else
+    {
+      return "Hey kiddo";
+    }
+  };
+
+
+  console.log(AgeCheck(18));
+  console.log(Greet());
+
+
+  const ProductVAT = (ProductPrice, VATPercetage) => {
+    let VATIncluded = CalculateVAT.call(this, ProductPrice, VATPercetage)
+    return VATIncluded;
+  }
+
+  const CalculateVAT = (ProductPrice,VATPercetage) =>{
+    return ProductPrice * (VATPercetage / 100) + ProductPrice;
+  }
+
+  const ProductVATReverse = (ProductPriceVAT, VATPercetage2) => {
+    let VATIncluded = CalculateVATReverse.call(this, ProductPriceVAT, VATPercetage2)
+    return VATIncluded;
+  }
+  
+  const CalculateVATReverse = (ProductPriceVAT,VATPercetage2) =>{
+    return ProductPriceVAT / (VATPercetage2 / 100 + 1);
+  }
+  console.log(ProductVATReverse(2.18,5));
+  console.log(ProductVAT(2,9));
